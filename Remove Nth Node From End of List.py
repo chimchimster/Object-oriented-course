d15a87505fffba0b1f8af9daf66a0c94dd3ebbc3
chimchimster <104686626+chimchimster@users.noreply.github.com>
2022-11-12 10:34:26 +0600

    Update Remove Nth Node From End of List.py

--- Remove Nth Node From End of List.py
+++ Remove Nth Node From End of List.py
@@ -1,39 +1,55 @@
+class ListNode(object):
+    def __init__ (self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution(object):
     def removeNthFromEnd (self, head, n):
+        x, y, z = head, head, head
         count_ = 0
-        tail = body = head
-        element_pop = None
+        length = self.__check_len(head)
+        index_ = abs(length - n - 1)
 
-        while head:
-            if count_ == (self.__check_len(head) - n):
-                if self.__check_len(head) == 2:
-                    head.next = tail
-                    return head
-                elif self.__check_len(head) == 1:
-                    head.next = None
-                    return head
-                elif self.__check_len(head) == 0:
-                    head = None
-                    return head
+        while x:
+            if count_ == index_:
+                if length in (n, 0):
+                    x = None
+                    return x
+                elif length == 1:
+                    x.next = None
+                    return x
                 else:
-                    element_pop = tail
-
-            head = head.next
-            tail = head
+                    break
 
             count_ += 1
 
-        result_ = body
+        count_ = 1
+        node = y
+        result = ListNode()
+        result.next = node
+        result.val = node.val
+        while node:
+            to_check_next = z.next
+            if count_ >= index_ - 1:
+                first_el = node
+                first_el.val = node.val
+                n = first_el.next = to_check_next.next
+                v = first_el.next.val = first_el.next.next.val
 
-        while result_:
-            if result_.next == element_pop:
-                body.next = result_.next.next
-            else:
-                body.next = result_
+                result.next = ListNode(v, n)
+                result.next = ListNode(val=0, next=None)
+                break
 
-            result_ = result_.next
-
-        return body
+            result.next = ListNode(node.val, node.next.next)
+            count_ += 1
+            
+            s = ''
+            while result.next:
+                s += str(result.next.val) + ' ,'
+                result.next = result.next.next
+            s = s.strip(', ')
+            print(f'[{s[::-1]}]')
 
     def __check_len (self, head):
         count = 0
@@ -41,5 +57,9 @@ class Solution(object):
         while head:
             head = head.next
             count += 1
-
         return count
+
+
+l = ListNode(6, ListNode(5, ListNode(4, ListNode(3, ListNode(2, ListNode(1, None))))))
+s = Solution()
+print(s.removeNthFromEnd(l, 2))
